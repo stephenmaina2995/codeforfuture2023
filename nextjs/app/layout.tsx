6b8da35ac42a89cdd437ac688f2f8e07f54faff5
laureac <laure.a.claret@gmail.com>
2023-11-30 14:23:05 +0100

    setup base template nextjs

---
+++ nextjs/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "FarmChain",
+  description:
+    "DApp that promotes transparency and traceability in the agricultural supply chain",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
