557d7525dc3d4d0e7bb4dfb71ce737d28c0ad17b
stephenmaina2995 <stephenmaina2995@gmail.com>
2023-12-01 11:56:55 +0300

    Added dependencies

---
+++ backend/AnimaPackage-React-MVipZ/src/screens/Box/Box.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+export const Box = () => {
+  return (
+    <div className="box">
+      <img className="rectangle" alt="Rectangle" src="/img/rectangle-1.png" />
+    </div>
+  );
+};
