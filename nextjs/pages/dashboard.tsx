691cc867b7b026b2166f0e66717372c2d9af8eac
laureac <laure.a.claret@gmail.com>
2023-12-02 18:38:28 +0100

    core: add env variables, listen for enent contract creation

--- nextjs/pages/dashboard.tsx
+++ nextjs/pages/dashboard.tsx
@@ -1,4 +1,30 @@
+import { ethers } from "ethers";
+import "dotenv/config";
+
 const Dashboard = () => {
+  const ownerAddress = "0x46235044283cd02df4212b774d6904cadee0ddde";
+
+  const provider = new ethers.JsonRpcProvider(process.env.NODE_URL);
+
+  const filter = {
+    fromBlock: 0,
+    toBlock: "latest",
+    topics: [
+      ethers.id("ContractCreated(address)"),
+      null,
+      ethers.zeroPadValue(ownerAddress, 32),
+    ],
+  };
+
+  provider
+    .getLogs(filter)
+    .then((logs) => {
+      logs.forEach((log) => {
+        console.log(`Contract found at address: ${log.address}`);
+      });
+    })
+    .catch((error) => console.error(error));
+
   return <>Dashboard</>;
 };
 
